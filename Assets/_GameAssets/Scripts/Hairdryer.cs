9a788c9a1ef56807b448f366ba2dec3443de0e5e
Salma_Besbes <besbes.salma@gmail.com>
2020-02-01 03:25:10 +0100

    **Changes** ``` - iceberg ```

---
+++ Assets/_GameAssets/Scripts/Hairdryer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hairdryer : MonoBehaviour
+{
+    SpriteRenderer sp;
+    Color c;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "iceberg")
+        {
+            if (sp ==null)
+            {
+                sp = collision.GetComponent<SpriteRenderer>();
+            }
+            else
+            {
+                c = sp.color;
+                c.a = c.a - 0.02f;
+                sp.color = c;
+            }
+        }
+    }
+}
