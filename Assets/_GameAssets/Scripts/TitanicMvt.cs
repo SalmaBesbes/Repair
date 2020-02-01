3b2acd4c60a7c0f42fb3209b89c0372679106251
Salma_Besbes <besbes.salma@gmail.com>
2020-02-02 00:26:41 +0100

    **Changes** ``` - Titanic mvt and scale ```

---
+++ Assets/_GameAssets/Scripts/TitanicMvt.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitanicMvt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOMove(new Vector3(40.9f, -2.7f, 90f), 35f);
+        transform.DOScale(new Vector3(1.2f, 1.2f, 1.2f), 8f);
+    }
+
+}
