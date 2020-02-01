e974f3b4ec31b40116fb954d7a6cf77a55bc2241
hizabri <hizabri.med@gmail.com>
2020-02-01 14:29:25 +0100

    Scene ThorWP Gauge
    Scene ThorWP Gauge

---
+++ Assets/_GameAssets/Scripts/ThorWpSlider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThorWpSlider : MonoBehaviour
+{
+
+    public static ThorWpSlider TWS;
+    private Slider gauge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TWS = this;
+        gauge = GetComponent<Slider>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.LogWarning(gauge.value);           
+        }       
+    }
+
+
+    
+
+
+
+}
