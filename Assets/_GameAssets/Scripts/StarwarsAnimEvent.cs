f8a1f63d3c7261ee21484f43fe9e27308a340da3
Salma_Besbes <besbes.salma@gmail.com>
2020-02-02 01:18:25 +0100

    **WIP** ``` - Star wars scene animation ```

---
+++ Assets/_GameAssets/Scripts/StarwarsAnimEvent.cs
@@ -0,0 +1,13 @@
+﻿using DoozyUI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarwarsAnimEvent : MonoBehaviour
+{
+    public void triggerPlayableScene()
+    {
+        UIManager.HideUiElement("swintro", "GGJ");
+        UIManager.ShowUiElement("blackfade", "GGJ");
+    }
+}
